3b4ff8c094d1b07cc4ed24ce435f91145cbde13b
Craig-WIT <20095351@mail.wit.ie>
2024-12-02 20:36:25 +0000

    Adding and styling AboutMe section

---
+++ src/lib/components/SectionHeader.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+    import type { Snippet } from "svelte";
+
+    interface SectionHeaderProps {
+        children: Snippet,
+        sectionName: string,
+    }
+
+    let { children, sectionName }:SectionHeaderProps = $props();
+</script>
+
+<div class="default-margin mt-l">
+    <h2 id={sectionName}>{@render children()}</h2>
+    <div class="underscore"></div>
+</div>
+
+
--- src/lib/components/index.ts
+++ src/lib/components/index.ts
@@ -1,2 +1,5 @@
 export {default as HeroSection} from './Sections/HeroSection.svelte'
+export {default as AboutMeSection} from './Sections/AboutMeSection.svelte'
 export {default as Button} from './Button.svelte'
+export {default as SectionHeader} from './SectionHeader.svelte'
+
