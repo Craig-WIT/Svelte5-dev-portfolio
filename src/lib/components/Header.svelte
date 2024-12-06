5890d0306580c4d867f2f1b5f3dff6ac3cada9eb
Craig-WIT <20095351@mail.wit.ie>
2024-12-06 22:03:22 +0000

    Adding a Header navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,3 +1,22 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import Button from "./Button.svelte";
+
+  function goToContactForm() {
+    goto('/#contact-form')
+  }
+
+</script>
+
+<navbar class="navbar default-margin">
+  <a href="/" class="logo">CG</a>
+  <div class="navbar-links">
+    <a href="/#about-me" class="navlink">About Me</a>
+    <a href="/#my-work" class="navlink">Work</a>
+    <Button className="nav-bar" onclick={goToContactForm}>Contact</Button>
+  </div>
+</navbar>
+
 <style>
   .navbar {
     display: flex;
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import Header from '$components/Header.svelte';
     import '../app.css'
 
     let {children} = $props();
 </script>
 
+<Header></Header>
 {@render children()}
